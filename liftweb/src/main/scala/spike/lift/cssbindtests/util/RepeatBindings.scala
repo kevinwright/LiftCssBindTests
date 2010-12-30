f1f188b9ac8b9a5dfce82f3260a33e5663e226f2
Kevin Wright <kev.lee.wright@gmail.com>
2010-12-30 14:42:57 +0000

    factored out RepeatBindings object

---
+++ liftweb/src/main/scala/spike/lift/cssbindtests/util/RepeatBindings.scala
@@ -0,0 +1,11 @@
+package spike.lift.cssbindtests.util
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kevin
+ * Date: 30/12/10
+ * Time: 14:39
+ * To change this template use File | Settings | File Templates.
+ */
+
+class RepeatBindings
